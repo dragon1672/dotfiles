d920b23a44f94b6fb1fc95d2fd4d75e4a9326751
Anthony Corbin <corbinan@google.com>
2015-12-29 11:34:21 -0800

    better prompt

--- bash/bashrc.d/bashrc.d/prompt_string.config.bash
+++ bash/bashrc.d/bashrc.d/prompt_string.config.bash
@@ -1,12 +1,21 @@
-# Colors terminal
-GREEN='\[\033[0;32m\]'
-BLUE='\[\033[0;36m\]'
-DEFAULTCOL='\[\e[m\]'
-# This is the prefix thingy
-export PS1="${BLUE}\h:\w\$ $DEFAULTCOL"
-#export PS1="${GREEN}\h:\W $DEFAULTCOL" # The green blended in too much
-PROMPT_DIRTRIM=3
 
-unset GREEN
-unset BLUE
-unset DEFAULTTCOL
+
+prompt() {
+
+ GREEN='\[\033[0;32m\]'
+ BLUE='\[\033[0;36m\]'
+ DEFAULTCOL='\[\e[m\]'
+ PROMPT_DIRTRIM=
+
+ if [ $# -eq 0 ] || [[ $* == *--default* ]]; then
+  export PS1="${BLUE}\h:\w\$ $DEFAULTCOL"
+  PROMPT_DIRTRIM=3
+elif [[ $* == *--google*  ]]; then
+  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\ $ '
+elif [[ $* == *--minimal*  ]]; then
+  PS1="$ $DEFAULTCOL"
+
+fi
+
+}
+prompt
