ac4b7f1a64e7f7ff0a187095b839b5726167b5d1
Anthony Corbin <corbinan@google.com>
2016-01-08 15:31:05 -0800

    better importer

--- importer.bash
+++ importer.bash
@@ -1,2 +1,19 @@
-$(dirname $0)/bash/importer
-$(dirname $0)/sh/importer
+currentDir=$(dirname $0)
+
+# takes in {dot file name} {directory to export to}
+generalImporter() {
+  local name=$1
+  local dir=$2
+  mkdir -p "$dir"
+  cp -f "$HOME/.$name" "$dir/$name"
+  if [[ -d "$HOME/$name.d/" ]]; then
+    rm -rf "$dir/$name.d"
+    mkdir -p "$dir/$name.d"
+    cp -r "$HOME/$name.d/" "$dir/"
+  fi
+}
+
+generalImporter bashrc "$currentDir/bash"
+generalImporter profile "$currentDir/bash"
+generalImporter inputrc "$currentDir/bash"
+generalImporter tmux.conf "$currentDir/tmux"
