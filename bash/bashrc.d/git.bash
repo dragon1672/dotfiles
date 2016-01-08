68b291c78ff824b0197e05fd19f163c3c828533a
Anthony Corbin <corbinan@google.com>
2016-01-08 13:46:41 -0800

    adding git check

--- bash/bashrc.d/git.bash
+++ bash/bashrc.d/git.bash
@@ -1,3 +1,8 @@
 gl() {
   git log --graph --abbrev-commit --pretty=oneline --decorate
 }
+
+# Test whether we're in a git repo
+is-git-repo() {
+  $(git rev-parse --is-inside-work-tree &> /dev/null)
+}
