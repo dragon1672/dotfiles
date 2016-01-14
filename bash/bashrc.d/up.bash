777509835d08cb2f529dfdd636f52d6c40a9612d
Anthony Corbin <dragon1672@gmail.com>
2016-01-14 07:05:49 -0800

    up with strings and examples

--- bash/bashrc.d/up.bash
+++ bash/bashrc.d/up.bash
@@ -1,13 +1,36 @@
 # Move up many directories
+
+#Examples ~/a/b/c/d/e $ up
+#         ~/a/b/c/d $
+
+#Examples ~/a/b/c/d/e $ up 3
+#         ~/a/b $
+
+#Examples ~/a/b/c/d/e $ up b
+#         ~/a/b $
+
+#Examples ~/a/b/c/d/e $ up b/c
+#         ~/a/b/c $
+
 up() {
   if [[ $@ == "" ]];
   then
     cd ..
   else
-    local path=''
-    for i in `seq 1 $@`; do
-      path="$path../"
-    done
-    cd $path
+    #are we dealing with a number
+    if [[ $@ =~ ^-?[0-9]+$ ]]; then
+      local path=''
+      for i in `seq 1 $@`; do
+        path="$path../"
+      done
+      cd $path
+    else #we are dealing with a string
+      local path=$PWD/
+      local foundFolder=${path#*$@*/}
+      local cdUp=${foundFolder//[^\/]}
+      cdUp=${#cdUp}
+      #lets go there
+      up $cdUp
+    fi
   fi
 }
