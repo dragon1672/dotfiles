7bfa8cf31d0c234aa784321e8c58ef818bf22f3e
Anthony Corbin <dragon1672@gmail.com>
2016-05-09 08:44:06 -0700

    making git smarter and removing need for 'git' prefix

--- bash/bashrc.d/git.bash
+++ bash/bashrc.d/git.bash
@@ -4,5 +4,24 @@ gl() {
 
 # Test whether we're in a git repo
 is-git-repo() {
-  $(git rev-parse --is-inside-work-tree &> /dev/null)
+$(git rev-parse --is-inside-work-tree &> /dev/null)
+}
+
+stash() {
+  is-git-repo && git stash "$@" || command stash
+}
+commit() {
+  is-git-repo && git commit "$@" || command commit
+}
+br() {
+  branch "$@"
+}
+branch() {
+  is-git-repo && git branch "$@" || command branch
+}
+st() {
+  is-git-repo && git st "$@" || command st
+}
+diff() {
+  is-git-repo && git diff "$@" || command diff
 }
