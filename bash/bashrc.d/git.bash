ff22fd1bdc87518272f24b01242b6868d2c3adb4
Anthony Corbin <dragon1672@gmail.com>
2016-05-23 10:13:12 -0700

    forwarding commands along

--- bash/bashrc.d/git.bash
+++ bash/bashrc.d/git.bash
@@ -8,26 +8,26 @@ $(git rev-parse --is-inside-work-tree &> /dev/null)
 }
 
 stash() {
-  is-git-repo && git stash "$@" || command stash
+  is-git-repo && git stash "$@" || command stash "$@"
 }
 commit() {
-  is-git-repo && git commit "$@" || command commit
+  is-git-repo && git commit "$@" || command commit "$@"
 }
 br() {
   branch "$@"
 }
 branch() {
-  is-git-repo && git branch "$@" || command branch
+  is-git-repo && git branch "$@" || command branch "$@"
 }
 st() {
-  is-git-repo && git st "$@" || command st
+  is-git-repo && git st "$@" || command st "$@"
 }
 diff() {
-  is-git-repo && git diff "$@" || command diff
+  is-git-repo && git diff "$@" || command diff "$@"
 }
 push() {
-  is-git-repo && git push "$@" || command push
+  is-git-repo && git push "$@" || command push "$@"
 }
 add() {
-  is-git-repo && git add "$@" || command add
+  is-git-repo && git add "$@" || command add "$@"
 }
